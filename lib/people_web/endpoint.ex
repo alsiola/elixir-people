95fc333ed429d60f99deff1d0eb939c3569571c0
Alex Young <alex.young@hive.hr>
2018-11-10 17:05:49 +0000

    Remove channels generated code
    Channels/web sockets aren't required for this application, so
    remove the generated code

--- lib/people_web/endpoint.ex
+++ lib/people_web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule PeopleWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :people
 
-  socket "/socket", PeopleWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
